5520756c0b13dee7bc8d43a1f5bd8ec29f456fba
Israel Alves <israel_alves77@hotmail.com>
2025-01-31 11:27:31 -0300

    feat: implementação de fluxo de criação pagamento pix e consulta de situação pagamento pix

---
+++ payment-service/src/main/java/com/sos/payment_service/models/PaymentRequest.java
@@ -0,0 +1,27 @@
+package com.sos.payment_service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class PaymentRequest {
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
